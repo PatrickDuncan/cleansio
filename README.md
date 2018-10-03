a4b29085514aaf150f4f8310aa79ebe8e8fc3889
Patrick Duncan <patrick.duncan@outlook.com>
2018-10-03 16:23:12 -0400

    Display lyrics from an audio file (#6)

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# cleansio
+# Cleansio
+
+## Usage
+```
+python cleansio.py <FLAC_PATH>
+```
+- FLAC_PATH
+    - Must be a .flac audio file
+    - It must be mono (as opposed to stereo)
+    - Must be less than 10 seconds
+    - The sample rate must be 44100
+
+### Requirements
+
+1. Posix shell
+
+## Setup
+
+1. Check the Requirements
+2. Clone this repo
+3. Install Python 3.4+
+    - [Anaconda is recommended](https://www.anaconda.com/download/)
+4. Download your Google Cloud Credentials JSON file
+5. Set the following environment variables:
+    ```
+    export GOOGLE_APPLICATION_CREDENTIALS=<PATH_TO_JSON>
+    ```
+6. Install the following Python libraries:
+    ```
+    pip install --upgrade google-cloud-speech
+    ```
+7. _(OPTIONAL)_ Install the [Google Cloud SDK](https://cloud.google.com/sdk/docs/)
+8. You're all set!
---
+++ cleansio.py
@@ -0,0 +1,29 @@
+import sys
+
+def transcribe_file(speech_file):
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    client = speech.SpeechClient()
+
+    with open(speech_file, 'rb') as audio_file:
+        content = audio_file.read()
+
+    audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=44100,
+        language_code='en-US')
+
+    response = client.recognize(config, audio)
+    for result in response.results:
+        print(result)
+
+def valid_input():
+    return(sys.argv[1].lower()[-1:-5] == '.flac')
+
+if __name__ == '__main__':
+    if valid_input:
+        transcribe_file(sys.argv[1])
+    else:
+        print("Please see the README.")
