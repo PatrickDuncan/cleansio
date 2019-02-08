1bec2eeb5ab8ebd0cdc361a17016e8198ce38607
Patrick Duncan <patrick.duncan@outlook.com>
2019-02-08 14:20:34 -0500

    Added librosa package

--- cleansio/audio/accuracy.py
+++ cleansio/audio/accuracy.py
@@ -2,7 +2,11 @@
     recognition. """
 
 def __maximize_volume(chunk):
+    """ Increase chunk's volume up to the point before clipping """
     return chunk - chunk.max_dBFS
+
+def __isolate_vocals(chunk):
+    return chunk
 
 def improve_accuracy(chunk):
     """ Filter chunk through various functions to improve speech recognition """
