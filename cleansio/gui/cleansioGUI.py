93c2190b916de8017972e41bb6879f4d6ad3a6bb
asijasd <asijasd88@gmail.com>
2019-03-21 17:08:38 -0400

    Added list of songs and file-picker. The file-picker doesn't do anything yet.

--- cleansio/gui/cleansioGUI.py
+++ cleansio/gui/cleansioGUI.py
@@ -1,15 +1,39 @@
-from appJar import gui # GUI
-from platform import system # system
+from appJar import gui # Used to create the GUI
+from platform import system # Used to determine which OS the script is running on
+from os.path import expanduser # Used to find the user' s home dir.
 
 # Create the GUI
 app = gui()
 
 # Handles a button press
 def press(button):
-	app.infoBox("Test", "Hello world!")
+	if button == "Censor File":
+		app.infoBox("Test", "Hello world!")
+	
+	elif button == "Realtime Censoring": # Realtime
+		app.infoBox("Info", "Only on Mac")
+
+	elif button == "Add Song": # Add songs to the list using a file picker
+		chosenFile = app.openBox("Choose a song", dirName=expanduser("~"), fileTypes=[("Audio files", "*.wav"), ("Audio Files", "*.mp3")])
+
+	else: # Unknown
+		app.infoBox("???", "How did this happen?")
 
 # Sets up the GUI
 def setupGUI():
+	### Start the left frame
+	app.startFrame("leftFrame", row=0, column=0)
+
+	## Add a labelled box to contain the conversion inputs
+	app.startLabelFrame("Songs to convert")
+	app.startScrollPane("convListScrollPane")
+	app.addListBox("convSongs", [])
+	app.stopScrollPane()
+	app.addButtons(["Add Song"], press)
+	app.stopLabelFrame()
+
+	app.stopFrame()
+
 	if system() == "Darwin": # Mac
 		app.addButtons(["Censor File", "Realtime Censoring"], press)
 
