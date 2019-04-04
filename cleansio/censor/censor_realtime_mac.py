426dd4b31a9e8fa32d8ff85b1f7baab2d89b6c97
Levin Noronha <levin.noro@gmail.com>
2019-04-04 01:59:33 -0400

    Use callback fn + in/out buffers to record + play

--- cleansio/censor/censor_realtime_mac.py
+++ cleansio/censor/censor_realtime_mac.py
@@ -21,36 +21,63 @@ class CensorRealtimeMac(Censor):
 
     """ Filters audio stream in real-time """
     def __init__(self, args, explicits):
+        print('Initialzed realtime censor object')
         super().__init__(explicits, args.output_encoding, args.output_location)
         self.__switch_audio_source()
         create_env_var('CLEANSIO_CHUNKS_LIST', '[]')
-        self.lock = threading.Lock()
         self.chunk_prefix = create_temp_dir() + time_filename() + '-'
-        self.all_chunks = []
-        self.clean_file_chunks = []
-        self.all_data = deque([])
-        self.clean_file = AudioSegment.empty()
+        self.audio_file = AudioSegment.empty()
+        self.processing_queue = []
+        self.processing_lock = threading.Lock()
+        self.playback_queue = []
+        self.playback_lock = threading.Lock()
 
     def censor(self):
         """ Censors audio chunks in a continuous stream """
         """ Creates a clean/new version of a file by removing explicits """
-
         samplerate = 44100  # Hertz
         duration = 5  # seconds
 
+        # Start thread that will analyze and censor recorded chunks
         processing_thread = threading.Thread(target=self.run)
         processing_thread.daemon = True
         processing_thread.start()
 
-        for i in range(0,10+1,5):
-            # Record
-            mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
-                            channels=1, blocking=True)
-            with self.lock:
-                self.all_data.append(mydata)
+        try:
+            # listen from Soundflower, play to speakers
+            with sd.Stream(device=(2, 1),
+                       samplerate=samplerate, blocksize=int(samplerate*duration),
+                       channels=1, callback=self.callback, finished_callback=self.finished_callback):
+                print('#' * 80)
+                print('press Return to stop censoring')
+                print('#' * 80)
+                input()
+        except KeyboardInterrupt:
+            print('\nInterrupted by user')
+            CensorRealtimeMac.running = False
+        except Exception as e:
+            print(type(e).__name__ + ': ' + str(e))
+            CensorRealtimeMac.running = False
 
-        CensorRealtimeMac.running = False
-        processing_thread.join()
+        # processing_thread.join()
+
+    def callback(self, indata, outdata, frames, time, status):
+        if status:
+            print(status)
+
+        #add to processing_queue
+        with self.processing_lock:
+            self.processing_queue.append(indata)
+        
+        # consume playback_queue
+        with self.playback_lock:
+            if len(self.playback_queue) > 0:
+                outdata[:] = self.playback_queue.pop(0)
+            else:
+                outdata.fill(0)
+
+    def finished_callback(self):
+        self.create_clean_file(self.audio_file)
 
     def run(self):
         index = 0
@@ -59,26 +86,28 @@ class CensorRealtimeMac(Censor):
             if (not CensorRealtimeMac.running):
                 break
 
-            if len(self.all_data) > 0:
+            processing_queue_length = 0;
+            with self.processing_lock:
+                processing_queue_length = len(self.processing_queue)
+
+            if processing_queue_length > 0:
+                print('processing_queue length='+str(processing_queue_length))
                 print('index={}'.format(index))
-                with self.lock:
-                    mydata = self.all_data.popleft()
+                with self.processing_lock:
+                    frames_to_process = self.processing_queue.pop(0)
 
                 # Write recording to file
                 file_path = self.chunk_prefix + str(index) +'.wav'
-                # print('normal_chunks filepath is {}'.format(file_path))
-                sf.write(file_path, mydata, 44100)
+                sf.write(file_path, frames_to_process, 44100)
                 self.__update_env_chunks_list(file_path)
                 # Create AudioSegment object from recording and append it to list
                 recorded_chunk = read_and_convert_audio(file_path)
-                self.all_chunks.append(file_path)
 
-                # Use first half of recorded chunk as start of overlapping chunk
+                # Use first half of recorded chunk to complete overlapping chunk
                 overlapping_chunk = overlapping_chunk_start + recorded_chunk[:2500]
                 overlapping_path = append_before_ext(file_path, '-overlapping')
                 convert_and_write_chunk(overlapping_chunk,overlapping_path,'wav')
                 self.__update_env_chunks_list(overlapping_path)
-                self.all_chunks.append(overlapping_path)
 
                 # Create next overlapping_start from second half of recorded chunk
                 overlapping_chunk_start = recorded_chunk[-2500:]
@@ -93,8 +122,16 @@ class CensorRealtimeMac(Censor):
                     overlapping_accuracy_chunk = improve_accuracy(overlapping_chunk)
                     convert_and_write_chunk(overlapping_accuracy_chunk,chunk_file,'wav')
 
-                self.clean_file_chunks.append(self.censor_audio_chunk(file_path))
+                clean_chunk = self.censor_audio_chunk(file_path)
+                clean_chunk_filepath = create_temp_dir() + 'clean_chunk.wav'
+                clean_chunk.export(clean_chunk_filepath, format='wav')
+
+                # Read and convert it to frames
+                clean_frames, sample_rate = sf.read(clean_chunk_filepath,dtype='float32',fill_value=0.0,frames=int(44100*5),always_2d=True)
+                with self.playback_lock:
+                    self.playback_queue.append(clean_frames)
                 index += 5
+                self.audio_file += clean_chunk
 
     @classmethod
     def __switch_audio_source(cls):
